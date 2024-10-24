3ec1bc0063a6e48007482b5a2cffd96ff69e7317
Глеб <126265406+DecibelProject@users.noreply.github.com>
2024-10-24 22:40:46 +0300

    Update server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const serverless = require('serverless-http');
 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js on Netlify!');
+});
+
+module.exports.handler = serverless(app);
